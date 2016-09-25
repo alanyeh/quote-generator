cbe110d4571a81568638765ea93c1fac0d63c210
Alan Yeh <alnyeh@gmail.com>
2016-09-25 13:57:53 -0700

    First commit

---
+++ css/reference.js
@@ -0,0 +1,8 @@
+reference 
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/", false);
+// Add your code below!
+xhr.send();
+console.log(xhr.status);
+console.log(xhr.statusText);
---
+++ scripts/designquotes.js
@@ -0,0 +1,415 @@
+var quotes = [
+"I am slowly learning that some people are not good for me, no matter how much I love them.",
+
+
+"You come to a point in your life when you really don't care what people think about you, you just care what you think about yourself.",
+
+
+"It eventually gets better, without any sort of explanation. You just wake up one morning & you're not as upset any.",
+
+
+"As you move forward in life, you may need to change your circle of friends. Everyone around you isn't interested in seeing you improve. ",
+
+
+"It's funny because we ask God to change our situation not knowing, He put us in that situation to change us. ",
+
+
+"Our generation has lost the value of romance, the value of trust, the value of conversation. Sadly small talk is the new deep. ",
+
+
+"True beauty doesn't come from appearance, but rather comes from within. Beautiful, is an open mind, a kind heart & a loving soul. ",
+
+
+"I think one of my favorite feelings is laughing with someone and realizing half way through how much you enjoy them and their existence.",
+
+
+"I find it really beautiful when someone prays for you without you knowing, I don't think there's any form of deeper & purer love. ",
+
+
+"Not everyone is meant to be in your future. Some people are just passing through to teach you lessons in life.",
+
+
+"Cleanse your mind of all bad vibes and let the good vibes flow in.",
+
+
+"True confidence has no room for jealously & envy. When you know you are great, you have no reason to hate. ",
+
+
+"You can forgive a person & not allow them back into your life.",
+
+
+"You can't change how people feel about you, so don't try. Just live your life and be happy. ",
+
+
+"Overthinking is the biggest cause of our unhappiness. Keep yourself occupied. Keep your mind off things that don't help you.",
+
+
+"Life is a balance of holding on and letting go.",
+
+
+"Someone else is praying for the things you take for granted.",
+
+
+"Don't let the drama, stress, and craziness of this world distract you from the beauty, bliss, and love that exists everywhere. ",
+
+
+"Count blessings, not problems. ",
+
+
+"I love you means nothing without action. Anyone can say it but only the ones who truly care about you will show you.",
+
+
+"Never give up, not because you still have tomorrow to try, but because you may not have tomorrow to try. ",
+
+
+"Don't let little things destroy your happiness.",
+
+
+"All that you accomplish or fail to accomplish with your life is the direct result of your thoughts.",
+
+
+"Stay lowkey & let them assume.",
+
+
+"Better things are coming.",
+
+
+"Forgive to ease the weight off of your heart.",
+
+
+"My life isn't perfect. But I'm thankful for everything I have.",
+
+
+"Life is too short to wake up with regrets. So love the people who treat you right and forget about the ones who don't.",
+
+
+"A negative mind will never give you a positive life.",
+
+
+"You know one of the most expensive things you could ever do is pay attention to the wrong people.",
+
+
+"The best things in life come with patience.",
+
+
+"Keep calm, don't lose what's in front of you because of temporary emotions. Anger passes. ",
+
+
+"I've found that growing up means being honest. About what I want. What I need. What I feel. Who I am.",
+
+
+"Choose the person who makes you better.",
+
+
+"Success is the only option.",
+
+
+"You're not the same person you were a year ago, a month ago, or a week ago. You're constantly changing; experiences don't stop. That's life.",
+
+
+"Life goal: Travel to as many places as I can.",
+
+
+"Different is attractive.",
+
+
+"No expectations, no disappointments. ",
+
+
+"I appreciate meaningful conversations that make me think.",
+
+
+"I appreciate honesty  than anything. If you tell me what it is up front, you gave me the option to take it or leave it & I respect th…",
+
+
+"So many people in this world have trust issues because people say I'm here for you & then run away as soon as things start to fall apart.",
+
+
+"Sometimes you have to accept the fact that certain things will never go back to how it used to be.",
+
+
+"Learning to be happy with what I have while working for what I want.",
+
+
+"Better things are coming.",
+
+
+"The reason why people give up so fast is because they tend to look at how far they still have to go, instead of how far they have come. ",
+
+
+"Don't stress about something you can't change.",
+
+
+"I love people who are open-minded. People who just vibe with whatever you talk about. You can talk about anything & everything.",
+
+
+"We get attached to temporary things, then wonder why our happiness never lasts. ",
+
+
+"Just a reminder that you're whole without someone else. You are not a fraction. You're a complete masterpiece all by yourself.",
+
+
+"I think it's important to realize you can miss something, but not want it back. ",
+
+
+"Surround yourself with people that make you better.",
+
+
+"Don't let someone else's bad attitude ruin your day.",
+
+
+"Our platform is for the youth. It not about us - it about helping people through difficult points in their life.",
+
+
+"Plotting on the events this summer with Kush",
+
+
+"On my worst day, I'm still living my dream.",
+
+
+"Everything that you're going through is preparing you for what you asked for.",
+
+
+"You're here for a reason. If you're comparing yourself to someone else, you're off your own path. Focus on your own path. ",
+
+
+"A positive mind towards everything will give you a happier life.",
+
+
+"I never changed. I just learned.",
+
+
+"Focus on YOUR goals.",
+
+
+"Never be afraid of change. You may lose something good, but you may gain something even better. ",
+
+
+"Be with someone who wants to invest in you, learn from you, see you win, support your visions & fall in love with you daily. ",
+
+
+"I appreciate people with good energy.",
+
+
+"Never limit yourself.",
+
+
+"Life is not about being better than someone else, it's about being better than you used to be & becoming who you are. ",
+
+
+"Prioritize your own happiness.",
+
+
+"Nothing  attractive than loyalty.",
+
+
+"Don't be afraid of change. You may end up losing something good, but you'll probably end up gaining something so much better. ",
+
+
+"A secret to happiness is letting every situation be what it is instead of what you think it should be, and then making the best of it. ",
+
+
+"Only invest in friendships that are mutually beneficial with substantial growth. do not undervalue your importance for anyone. Be selective.",
+
+
+"Sometimes the bad things that happen in our lives put us directly on the path to the best things that will ever happen to us.",
+
+
+"I never lose. Either I win or I learn.",
+
+
+"I have the deepest affection for intellectual conversations. The ability to just sit and talk about love, life, anything, and everything.",
+
+
+"Don't give up now. Keep going.",
+
+
+"When things go my way, I thank God for the blessing. When things go wrong, I thank God for the lesson. ",
+
+
+"Don't carry your mistakes around with you. Instead, place them under your feet and use them as stepping stones to rise above them. ",
+
+
+"Be fearless when it comes to life, and careless when it comes to what people say and think about you. ",
+
+
+"Two things define you: Your patience when you have nothing, and your attitude when you have everything. ",
+
+
+"Take a second to think about how blessed you are.",
+
+
+"I pray that I always stay focused on the bigger picture, and never lose sight of my goals. Amen. ",
+
+
+"Being genuinely happy has little to do with other people, and everything to do with yourself. It's a personal choice.",
+
+
+"Your goals won't become a reality until you make it a priority. Learn to prioritize, and stay focused on what matters. ",
+
+
+"I love being around people who have a purpose and who know what they want and what life is about.",
+
+
+"I pray that everything works out in my favor and a list of blessings continue to come my way.",
+
+
+"Just trying to become a better me daily, whether it's baby steps or huge milestones, I'm making progress & growing. ",
+
+
+"Trying to become a better person in all aspects of my life. Spiritually, emotionally, & mentally. ",
+
+
+"I'm so very grateful for the place that I'm at in my life and I know it's only getting better from here.",
+
+
+"Never underestimate a person with a prayer and a plan.",
+
+
+"Stay in your own lane. Watch how much you get accomplished when you don't have time being distracted by everyone's progress. Stay focused.",
+
+
+"Be grateful, for everything. Your lows are someone else's highs. The very things you take for granted someone else is praying for. ",
+
+
+"Be thankful for all the struggles you go through. They make you stronger, wiser and humble. Don't let them break you, let them make you.",
+
+
+"I thank God for protecting me from what I  thought I wanted and blessing me with what I didn't know I needed.",
+
+
+"Don't fight for someone's attention. If they really care, you shouldn't have to. ",
+
+
+"Never forget who was with you from the start.",
+
+
+"What you think is either helping you or hindering you. Don't be afraid to challenge your belief. Question everything.",
+
+
+"Be who you are, and say what you feel. It's not being rude, it's being real. ",
+
+
+"You'll be fine. Feeling unsure & lost is part of your path. Don't avoid it. Breathe. You'll be okay even if you don't feel it all the time.",
+
+
+"If you love someone, pray for them. Pray for their peace. Pray for their growth. Pray for their success. Pray for their happiness.",
+
+
+"This summer I just want to clear my mind, and clear my life of all toxic, unsupportive and negative people. ",
+
+
+"You have absolutely no obligation to explain anything to anyone if your intentions were honest/pure and they misread your actions.",
+
+
+"I appreciate people with good energy.",
+
+
+"Be patient. Good things take time.",
+
+
+"Great things are coming.",
+
+
+"You gotta do what's best for you.",
+
+
+"Pay attention to how people act when you're not on good terms.",
+
+
+"You are responsible for your own happiness. If you expect others to make you happy, you will always be disappointed. ",
+
+
+"I just wanna go on  adventures. Be around good energy. Connect with people. Learn new things. Grow.",
+
+
+"When things go my way, I thank God for the blessing. When things go wrong, I thank God for the lesson. ",
+
+
+"You'll end up real disappointed if you think people will do for you as you do for them. Not everyone has the same heart as you.",
+
+
+"It costs $0.00 to be nice to people.",
+
+
+"You can get a lot of things back, but you can't get time back. You can't get back life. Time is priceless. Don't waste it. Make it count.…",
+
+
+"You don't need someone to complete you. You are complete. You need someone to be complete with. That is the meaning of friendship and love.",
+
+
+"There's nothing  attractive than a person with ambition. Someone with passion who knows what they want. Someone in love with them self.",
+
+
+"I don't force friendships nor do I force relationships. Either you rocking with me or you're not. I have  important things to focus on.",
+
+
+"Life is better when you're laughing.",
+
+
+"You can't change how people feel about you, so don't try. Just live your life and be happy. ",
+
+
+"Don't make permanent decisions with temporary feelings.",
+
+
+"Give yourself permission to walk away from anything that gives you bad vibes. There is no need to explain it. Just trust what you feel.",
+
+
+"Eventually all things fall into place. Until then, laugh at the confusion, live for the moments, and know everything happens for a reason…",
+
+
+"Never apologize for being you.",
+
+
+"You'll end up really disappointed if you think people will do for you as you do for them. Not everyone has the same heart as you.",
+
+
+"Open your mind before your mouth.",
+
+
+"Be thankful for the bad things in life.They open your eyes to see the good things you weren't paying attention to before.",
+
+
+"Be thankful for the bad things in life. They open your eyes to see the good things you weren't paying attention to before.",
+
+
+"No matter how much it hurts now, someday you will look back and realize your struggles changed your life for the better. ",
+
+
+"Never give up on something you really want. It's difficult to wait, but worse to regret. ",
+
+
+"Sometimes the wrong choices bring us to the right places.",
+
+
+"Let go of the negativity, focus on the good.",
+
+
+"Letting go is so freeing.",
+
+
+"Patience is everything when blessings are coming your way.",
+
+
+"I feel so blessed.",
+
+
+"No regrets. Just lessons learned.",
+
+
+"Happiness is a choice , not a result. Nothing will make you happy until you choose to be happy.",
+];
+
+function randomQuote() {
+	var rand = quotes[Math.floor(Math.random() * quotes.length)];
+	var getElement = document.getElementById("quote");
+	getElement.innerHTML = rand;
+};
+
+setInterval(function() {
+	randomQuote().fadeOut("slow");
+}, 5000);
+
+
+randomQuote();
